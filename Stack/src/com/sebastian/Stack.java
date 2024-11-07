68202514ffba919ba80707434c866ba4a481134d
Dimmer6985 <sebastian.corporan@upr.edu>
2024-11-07 08:37:04 -0400

    Implement all methods to 'Stack' interface

---
+++ Stack/src/com/sebastian/Stack.java
@@ -0,0 +1,53 @@
+package com.sebastian;
+
+import java.util.EmptyStackException;
+
+/**
+ * Represents a generic Stack data structure.
+ * 
+ * @author Sebastian L. Corporan Berrios
+ */
+public interface Stack<E> {
+
+    /**
+     * Pushes an element onto the top of the stack.
+     * 
+     * @param elem The element to be inserted
+     */
+    public void push(E elem);
+
+    /**
+     * Removes the element at the top of the stack
+     * 
+     * @throws EmptyStackException - If the stack is empty.
+     */
+    public void pop();
+
+    /**
+     * Looks at the element at the top of the stack without removing it from the
+     * stack.
+     * 
+     * @return the element at the top of the stack
+     * 
+     * @throws EmptyStackException - If the stack is empty.
+     */
+    public E peek();
+
+    /**
+     * Searches for the specified element in the stack.
+     * 
+     * @param elem - The element to search in the stack.
+     * @return The position in which the element is located, -1 otherwise.
+     * 
+     * @throws EmptyStackException - If the stack is empty.
+     */
+    public int search(E elem);
+
+    /**
+     * Determines if the stack is empty.
+     * 
+     * @return True if the stack contains no elements; false otherwise.
+     */
+    public boolean isEmpty();
+
+} // interface
